474fbe74584bd09a8dd0f5f41f628cc18bb0d724
SioneIII <186331518+SioneIII@users.noreply.github.com>
2024-10-31 00:00:00 +0500

    'add file graph.h'

---
+++ graph.h
@@ -0,0 +1,9 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#define MAX_VERTICES 10
+
+void printGraph(int graph[MAX_VERTICES][MAX_VERTICES], int vertices);
+void addEdge(int graph[MAX_VERTICES][MAX_VERTICES], int src, int dest);
+
+#endif
